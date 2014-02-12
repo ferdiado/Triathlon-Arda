0f4435f05c43b0d044c2651dddf2ce54622cd489
Luis Garcia Figueres <garciafigueres@gmail.com>
2014-02-12 17:20:12 +0100

    Creo el paquete com.arda.borrame y la clase Borrame.java. Simplemente quiero comprobar el funcionamiento de GitHub.

---
+++ Triathlon/src/com/arda/borrame/Borrame.java
@@ -0,0 +1,12 @@
+/**
+ * @author Luis
+ * @version 1.0
+ * 
+ * Subo este paquete y esta clase simplemente para comprobar el funcionamiento de GitHub.
+ * 
+ */
+package com.arda.borrame;
+
+public class Borrame {
+
+}
